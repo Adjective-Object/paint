c4327049a9dfc5d8ea408366b4008775dd2a4d27
PurpeTengu <mhuan13@gmail.com>
2013-09-28 10:23:09 -0400

    directions on player

--- entities.py
+++ entities.py
@@ -4,9 +4,6 @@ import os
 import time
 import random
 
-placeholder_graphic = pygame.image.load(os.getcwd()+"/res/player.png")
-placeholder_graphic.set_colorkey(pygame.color.Color(255,255,255))
-
 def get_rect_overlap(rect1, rect2):
      return (
         rect1.right-rect2.left if rect1.centerx<rect2.centerx else -(rect2.right - rect1.left),
@@ -114,49 +111,62 @@ class LivingEntity(Entity):
           self.facing = 2*get_sign(dy-self.y)   
 
 class Player(LivingEntity):
-    
-    SPEED = 1000
-    
-    def __init__(self,x,y,color,
-                 keybindings = {"LEFT": pygame.K_LEFT,
+     placeholder_graphic = pygame.image.load(os.getcwd()+"/res/player.png")
+     placeholder_graphic.set_colorkey(pygame.color.Color(255,255,255)) 
+     
+     placeholders = [
+           placeholder_graphic.subsurface(pygame.rect.Rect(0,0,50,50)),
+           placeholder_graphic.subsurface(pygame.rect.Rect(0,50,50,50)),
+           placeholder_graphic.subsurface(pygame.rect.Rect(0,100,50,50)),
+           placeholder_graphic.subsurface(pygame.rect.Rect(0,150,50,50)),
+     ]
+     
+     SPEED = 1000
+     
+     def __init__(self,x,y,color,
+                  keybindings = {"LEFT": pygame.K_LEFT,
                                 "RIGHT": pygame.K_RIGHT,
                                 "UP": pygame.K_UP,
                                 "DOWN": pygame.K_DOWN,
                                 "PAINT": pygame.K_z,
                                 "BOMB": pygame.K_x,
                                 "ITEM": pygame.K_c}):
-        super(Player,self).__init__(x,y)
-        self.color = color
-        self.keybindings = keybindings
-        self.friction = 0.9
-        self.size = Point(20,20)
-        self.rect_offset = Point(15,10)
-    
-    def update(self,elapsed):
-        super(Player,self).update(elapsed)
-        
-        if(self._pressed("LEFT")):
-            self.velocity.x -= Player.SPEED * elapsed
-        if(self._pressed("RIGHT")):
-            self.velocity.x += Player.SPEED * elapsed
-        if(self._pressed("UP")):
-            self.velocity.y -= Player.SPEED * elapsed
-        if(self._pressed("DOWN")):
-            self.velocity.y += Player.SPEED * elapsed
-        
-        if(self._pressed("PAINT")):
-            self._get_tile().paint_color = self.color
-    
-    def render(self,canvas):
-        canvas.blit(placeholder_graphic,
-                    (self.x - maingame.camerax,
-                     self.y - maingame.cameray -
-                         placeholder_graphic.get_size()[1])
-                    )
-    
-    def _pressed(self,code):
-        return pygame.key.get_pressed()[self.keybindings[code]]
-             
+          super(Player,self).__init__(x,y)
+          self.color = color
+          self.keybindings = keybindings
+          self.friction = 0.9
+          self.size = Point(20,20)
+          self.rect_offset = Point(15,10)
+      
+     def update(self,elapsed):
+          super(Player,self).update(elapsed)
+          
+          if(self._pressed("LEFT")):
+              self.velocity.x -= Player.SPEED * elapsed
+              self.facing = 3
+          if(self._pressed("RIGHT")):
+              self.velocity.x += Player.SPEED * elapsed
+              self.facing = 1
+          if(self._pressed("UP")):
+              self.velocity.y -= Player.SPEED * elapsed
+              self.facing = 2
+          if(self._pressed("DOWN")):
+              self.velocity.y += Player.SPEED * elapsed
+              self.facing = 0
+          
+          if(self._pressed("PAINT")):
+              self._get_tile().paint_color = self.color
+      
+     def render(self,canvas):
+          canvas.blit(Player.placeholders[self.facing],
+                      (self.x - maingame.camerax,
+                       self.y - maingame.cameray -
+                           Player.placeholders[self.facing].get_size()[1])
+                      )
+      
+     def _pressed(self,code):
+          return pygame.key.get_pressed()[self.keybindings[code]]
+               
 
 class Police(LivingEntity):
      
