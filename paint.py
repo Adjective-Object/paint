037a813599c6e547d3c410ec33e8a145cf1dbc9e
PurpeTengu <mhuan13@gmail.com>
2013-09-28 20:02:41 -0400

    menu states

--- paint.py
+++ paint.py
@@ -2,7 +2,10 @@ import pygame
 import time
 import sys
 import os
+import sys
+from about import AboutScreen
 from maingame import MainGame
+from mainmenugame import MainMenu
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
@@ -16,19 +19,38 @@ def do_game():
     
     screen_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Paints")
+    
+    menu = MainMenu(screen_surface)
+    about = AboutScreen(screen_surface)
     maingame = MainGame(screen_surface)
     
-    """
-    loop(screen_surface,
-         lambda elapsed: maingame.easing(elapsed/2),#main loop of easing
-         lambda : maingame.render(),#rendering game
-         lambda initial: (time.time()-initial)<=2)#ease for 2s
-    """ 
-    pygame.mixer.music.load(os.getcwd()+"/res/song.wav")
-    pygame.mixer.music.play(-1)
-    loop(screen_surface,
-         lambda elapsed: maingame.main_loop(elapsed),#main loop of game
-         lambda : maingame.render()) #rendering it
+    #pygame.mixer.music.load(os.getcwd()+"/res/song.wav")
+    #pygame.mixer.music.play(-1)    
+    while True:
+        menu.exitTo = None
+        about.exit = False
+        loop(screen_surface,
+             lambda elapsed: menu.main_loop(elapsed),#main loop of game
+             lambda : menu.render(),
+             lambda t : menu.exitTo is None) #rendering it
+        if(menu.exitTo == "2p"):
+            #TODO this
+            loop(screen_surface,
+                 lambda elapsed: maingame.main_loop(elapsed),#main loop of game
+                 lambda : maingame.render()) #rendering it
+        if(menu.exitTo == "4p"):
+            #TODO this
+            loop(screen_surface,
+                 lambda elapsed: maingame.main_loop(elapsed),#main loop of game
+                 lambda : maingame.render()) #rendering it
+        if(menu.exitTo == "about"):
+            loop(screen_surface,
+                 lambda elapsed: about.main_loop(elapsed),#main loop of about screen
+                 lambda : about.render(),
+                 lambda t : not about.exit) #rendering it       
+    pygame.quit()
+    sys.exit()
+   
     
 
 def loop(screen_surface, loopfn, dorender, loopcontrol=lambda initialTime: True ):
